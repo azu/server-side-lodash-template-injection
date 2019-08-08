efb78c75c96ccb3751e2eb1bab5cae651f8c97b0
azu <azuciao@gmail.com>
2019-08-08 17:22:37 +0900

    update

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const app = express();
 app.get('/', (req, res) => {
   res.set('Content-Type', 'text/html');
   const name = req.query.name
-  // Create a template from user input
   const compiled = _.template("Hello <%- name %>.");
   res.status(200).send(compiled({ name }));
 });
