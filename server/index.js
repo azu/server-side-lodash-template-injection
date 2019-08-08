ebc5f1c62f64c1c676c605c050a8287beb8d0d72
azu <azuciao@gmail.com>
2019-08-08 17:21:34 +0900

    fix: fix server side template injection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const _ = require('lodash');
-const escapeHTML = require('escape-html');
 const app = express();
 app.get('/', (req, res) => {
   res.set('Content-Type', 'text/html');
   const name = req.query.name
   // Create a template from user input
-  const compiled = _.template("Hello " + escapeHTML(name) + ".");
-  res.status(200).send(compiled());
+  const compiled = _.template("Hello <%- name %>.");
+  res.status(200).send(compiled({ name }));
 });
 
 module.exports = app;
